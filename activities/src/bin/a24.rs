e416e88b1d45536d2526e8ad6166f91b3bd472aa
Graeme Fox <ac.graeme@gmail.com>
2022-08-22 21:40:06 -0600

    updates to activities

--- activities/src/bin/a24.rs
+++ activities/src/bin/a24.rs
@@ -10,4 +10,18 @@
 
 fn main() {
     let data = vec![1, 2, 3, 4, 5];
+
+    let data_triple: Vec<_> = data
+        .iter()
+        .map(|num| num * 3)
+        .collect();
+
+    let over_ten: Vec<_> = data_triple
+        .iter()
+        .filter(|num| num > &&10)
+        .collect();
+
+    for n in over_ten {
+        println!("{:?}", n)
+    }
 }
