39ddcd3168acdb1b2fcbac448a8681995ea2aeb4
Graeme Fox <ac.graeme@gmail.com>
2022-09-04 10:34:37 -0700

    update to a34

--- activities/src/bin/a34.rs
+++ activities/src/bin/a34.rs
@@ -16,4 +16,62 @@
 // Notes:
 // * Optionally use generics for each state
 
-fn main() {}
+#[derive(Copy, Clone)]
+struct LuggageId(usize);
+struct Luggage(LuggageId);
+
+// impl <State> Luggage<State> {
+//     fn transition <NextState>(self, state: NextState) -> Luggage<NextState> {
+//         Luggage {
+//             id: id,
+//             state: state
+//         }
+//     } 
+// }
+
+struct CheckIn(LuggageId);
+struct OnLoading(LuggageId);
+struct Offloading(LuggageId);
+struct AwaitingPickup(LuggageId);
+struct EndCustody(LuggageId);
+
+impl Luggage {
+    fn new (id: LuggageId) -> Self {
+        Luggage (id)
+    }
+
+    fn check_in(self) -> CheckIn {
+        CheckIn(self.0)
+    }
+}
+
+impl CheckIn {
+    fn onload(self) -> OnLoading {
+        OnLoading(self.0)
+    }
+}
+
+impl OnLoading {
+    fn offload(self) -> Offloading {
+        Offloading(self.0)
+    }
+}
+
+impl Offloading {
+    fn await_pickup(self) -> AwaitingPickup {
+        AwaitingPickup(self.0)
+    }
+}
+
+impl AwaitingPickup {
+    fn pickup(self) -> (Luggage, EndCustody) {
+        (Luggage(self.0), EndCustody(self.0))
+    }
+    
+}
+
+fn main() {
+    let id = LuggageId(23);
+    let luggage = Luggage::new(id);
+    let luggage = luggage.check_in().onload().offload().await_pickup().pickup();
+}
