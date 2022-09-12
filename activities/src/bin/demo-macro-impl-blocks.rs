b26040195e6795d2b490a40198f794f9d8340d37
Graeme Fox <ac.graeme@gmail.com>
2022-09-12 15:37:14 -0400

    macro update

--- activities/src/bin/demo-macro-impl-blocks.rs
+++ activities/src/bin/demo-macro-impl-blocks.rs
@@ -1,4 +1,4 @@
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct Volume(usize);
 
 trait ReagentContainer {
@@ -6,21 +6,62 @@ trait ReagentContainer {
     fn current_volume(&self) -> Volume;
 }
 
+macro_rules! impl_regent_container {
+    ($container:ty : $volume:literal) => {
+        impl ReagentContainer for $container {
+            fn max_volume(&self) -> Volume {
+                Volume($volume)
+            }
+        
+            fn current_volume(&self) -> Volume {
+                self.current_volume
+            }
+        }
+    };
+}
+
+#[derive(Clone, Copy, Debug)]
 struct TallFlask {
     current_volume: Volume,
 }
 
+#[derive(Clone, Copy, Debug)]
 struct TestTube {
     current_volume: Volume,
 }
 
+#[derive(Clone, Copy, Debug)]
 struct Pipette {
     current_volume: Volume,
 }
 
+
+#[derive(Clone, Copy, Debug)]
 struct OtherTube {
     current_volume: Volume,
     max_volume: Volume,
 }
 
-fn main() {}
+impl ReagentContainer for OtherTube {
+    fn max_volume(&self) -> Volume {
+        self.max_volume
+    }
+
+    fn current_volume(&self) -> Volume {
+        self.current_volume
+    }
+}
+
+impl_regent_container!(TallFlask: 32);
+impl_regent_container!(TestTube: 10);
+impl_regent_container!(Pipette: 4);
+
+
+
+fn main() {
+    let tube = TestTube {
+        current_volume: Volume(0),
+    };
+
+    dbg!(tube.max_volume());
+}
