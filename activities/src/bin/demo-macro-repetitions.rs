932085979a51ac9b20220dc235feb562f148ac22
Graeme Fox <ac.graeme@gmail.com>
2022-09-12 16:28:24 -0400

    macro repetition update

--- activities/src/bin/demo-macro-repetitions.rs
+++ activities/src/bin/demo-macro-repetitions.rs
@@ -1,1 +1,16 @@
-fn main() {}
+macro_rules! myvec {
+    (
+        $( $element:expr ),+
+        $(,)?
+    ) => {{
+        let mut v = Vec::new();
+        $(
+        v.push($element);
+        )+
+        v
+    }};
+}
+
+fn main() {
+    let v = myvec![1,2,3,4];
+}
